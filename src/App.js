c0849328fe7b5964eca7c8bf54e0b925418f504f
ZuhebAkhter <zuheb1998@gmail.com>
2023-08-04 00:55:43 +0530

    addingredirects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import {  Route, Routes } from 'react-router-dom';
+import {  Route, Routes,useNavigate } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext, useEffect } from 'react';
+import AuthContext from './store/AuthContext';
 
 function App() {
+  
+  const authCtx=useContext(AuthContext)
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage/>}>
         </Route>
-        <Route path='/auth' element={<AuthPage />}>
+       {!authCtx.isLoggedIn && (<Route path='/auth' element={<AuthPage />}>
           
-        </Route>
-        <Route path='/profile' element={<UserProfile />}>
+        </Route>)}
+        {authCtx.isLoggedIn &&( <Route path='/profile' element={<UserProfile />}>
           
+        </Route>)}
+        <Route path='*' element={<AuthPage/>}>
         </Route>
         </Routes>
     </Layout>
